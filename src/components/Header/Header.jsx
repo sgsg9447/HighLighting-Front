3f3b8991c3cbab1ed6e23be355d6b19b8d24bc13
Lenol <ek3434@naver.com>
2022-02-21 03:42:51 +0900

    모달창 외부 클릭시 꺼지는 이벤트 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,33 @@ import { GiFilmProjector } from "react-icons/gi";
 import { FiLogIn } from "react-icons/fi";
 import { GoSignIn } from "react-icons/go";
 import { useHistory } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Modal from "./Modal";
 
 export default function Header() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   const history = useHistory();
+
+  const handleClickOutside = ({ target }) => {
+    console.log(target.className);
+    if (modalOpen && target.className === "openModal modal")
+      setModalOpen(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", handleClickOutside);
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  });
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   const onClickEvent = (e) => {
     const target = e.target.id;
@@ -28,14 +52,24 @@ export default function Header() {
           <GiFilmProjector /> HiGHLIGHTING
         </li>
         <div className="SignTool">
-          <li id="SignIn" onClick={onClickEvent}>
+          <li id="SignIn" onClick={openModal}>
             <GoSignIn /> 회원가입
           </li>
-          <li id="LogIn" onClick={onClickEvent}>
+          <li id="LogIn" onClick={openModal}>
             <FiLogIn /> 로그인
           </li>
         </div>
       </ul>
+      {modalOpen && (
+        <Modal
+          // ref={modalEl}
+          open={modalOpen}
+          close={closeModal}
+          Header="회원가입 및 로그인 "
+        >
+          여기가 회원가입 및 로그인창이 떠야될 부분.
+        </Modal>
+      )}
     </div>
   );
 }
