b57688d4aceb8c7c83fc146f6043811fbf22410c
Lenol <ek3434@naver.com>
2022-02-22 05:13:09 +0900

    merge Conflict solve 1회차

--- src/components/editor/VideoPlayer.jsx
+++ src/components/editor/VideoPlayer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
+
 import EditorTimePointerContext from "../../contexts/EditorTimePointerContext";
 import Player from "./in_VideoPlayer/Player";
 import "./VideoPlayer.scss";
-
 function VideoPlayer() {
   const { pointer, changePointer } = React.useContext(EditorTimePointerContext);
   const [play, setPlay] = React.useState(true);
@@ -13,7 +13,6 @@ function VideoPlayer() {
 
   return (
     <div className="VideoViewerContainer">
-      <h1>비디오 플레이어 영역</h1>
       <Player playing={play} />
       <h2>Time Pointer = {pointer}</h2>
       <h3>영상 isPlaying 시 Time Pointer 매 초 변경</h3>
