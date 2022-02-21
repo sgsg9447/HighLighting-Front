5073df1e736cb5263a03baa942b702dcb59a85d8
Lenol <ek3434@naver.com>
2022-02-22 04:53:29 +0900

    Editor 구성 컴포넌트 scss 추가. padding만 추가

--- src/components/editor/ChatViewer.jsx
+++ src/components/editor/ChatViewer.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-
-import EditorTimePointerContext from  "../../contexts/EditorTimePointerContext";
-
+import EditorTimePointerContext from "../../contexts/EditorTimePointerContext";
+import "./ChatViewer.scss";
 function ChatViewer() {
-  const {pointer, changePointer} = React.useContext(EditorTimePointerContext);
+  const { pointer, changePointer } = React.useContext(EditorTimePointerContext);
   return (
-    <div>
+    <div className="ChatViewerContainer">
       <h1>채팅 뷰어 영역</h1>
       <h2>Time Pointer = {pointer}</h2>
-      <button onClick={() => {changePointer(pointer)}}>
+      <button
+        onClick={() => {
+          changePointer(pointer);
+        }}
+      >
         얘가 바꿀일은 없음
       </button>
     </div>
--- src/components/editor/DataChart.jsx
+++ src/components/editor/DataChart.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-
 import EditorTimePointerContext from "../../contexts/EditorTimePointerContext";
-
+import "./DataChart.scss";
 function DataChart() {
   const { pointer, changePointer } = React.useContext(EditorTimePointerContext);
   return (
-    <div>
+    <div className="DataChartContainer">
       <h1>데이터 차트 영역</h1>
       <h2>Time Pointer = {pointer}</h2>
       <h3>유저 클릭 위치에 해당하는 값으로 Time Pointer 변경</h3>
-      <button onClick={() => {changePointer(pointer + 1)}}>
+      <button
+        onClick={() => {
+          changePointer(pointer + 1);
+        }}
+      >
         GET
       </button>
     </div>
