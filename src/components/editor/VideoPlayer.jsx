7f5e6c9eef83a3de8a2a46100ef98fc89f0ac324
Eithea <oguraff94@gmail.com>
2022-02-23 03:49:42 +0900

    merge CSS

--- src/components/editor/VideoPlayer.jsx
+++ src/components/editor/VideoPlayer.jsx
@@ -8,23 +8,6 @@ function VideoPlayer({url}) {
       <div className="VideoPlayer">
         <Player url={url} />
       </div>
-      <h2>Time Pointer = {pointer}</h2>
-      <h3>영상 isPlaying 시 Time Pointer 매 초 변경</h3>
-      <button
-        onClick={() => {
-          changePointer(pointer + 1);
-        }}
-      >
-        버튼
-      </button>
-      <button
-        onClick={() => {
-          handlePlayPause(play);
-        }}
-      >
-        재생/정지
-      </button>
-
     </div>
   );
 }
