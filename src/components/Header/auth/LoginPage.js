b67f62ce4f20760e8a84c610a6be936e16bdb5a8
Lenol <ek3434@naver.com>
2022-02-28 22:39:31 +0900

    로그인 컴포넌트 수정,scss 추가

--- src/components/Header/auth/LoginPage.js
+++ src/components/Header/auth/LoginPage.js
@@ -1,28 +1,72 @@
-import React from "react";
-
-// const [id, setId] = useState("");
-// const [pwd, setPwd] = useState("");
+import React, { useState } from "react";
+import "./LoginPage.scss";
 
 const LoginPage = () => {
+  const [id, setId] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  const loginHandler = (e) => {
+    if (e.target.id === "ID") {
+      setId(e.target.value);
+      console.log(id);
+    }
+    if (e.target.id === "PASSWORD") {
+      setPwd(e.target.value);
+      console.log(pwd);
+    }
+  };
+
+  const login = () => {
+    console.log("id :", id, "pwd :", pwd);
+  };
+
   return (
-    <div>
-      로그인 페이지
+    <div className="LoginContainer">
       <div>
-        <label for="ID">ID</label>
+        <label id="ID" htmlFor="ID">
+          ID
+        </label>
         <br />
-        <input id="ID" />
+        <input
+          id="ID"
+          className="loginID"
+          placeholder="아이디"
+          type="text"
+          onChange={loginHandler}
+        />
       </div>
       <div>
-        <label for="PASSWORD">PASSWORD</label>
+        <label htmlFor="PASSWORD">PASSWORD</label>
         <br />
-        <input id="PASSWORD" />
+        <input
+          className="loginPassWord"
+          id="PASSWORD"
+          placeholder="비밀번호"
+          tpye="password"
+          onChange={loginHandler}
+        />
       </div>
-      <button>회원 가입</button>
-      <button>로그인</button>
-      <br />
-      <button>Ouath 로그인 1번</button>
-      <br />
-      <button>Ouath 로그인 2번</button>
+      <div className="loginMid">
+        <label className="autoLogin" htmlFor="hint">
+          {" "}
+          <input type="checkbox" id="hint" /> 로그인 유지하기
+        </label>
+        <div className="autoLogin">아이디/비밀번호 찾기</div>
+      </div>
+      <button className="loginBtn" onClick={login}>
+        {" "}
+        로그인{" "}
+      </button>
+      <div className="socialBox">
+        <div className="kakao">
+          <img className="kakaoLogo" src={require("./Image/kakaotalk.png")} />
+          <div className="kakaoText">카카오 계정으로 신규가입</div>
+        </div>
+        <div className="facebook">
+          <img className="facebookLogo" src={require("./Image/facebook.png")} />
+          <div className="facebookText">페이스북 계정으로 신규가입</div>
+        </div>
+      </div>
     </div>
   );
 };
