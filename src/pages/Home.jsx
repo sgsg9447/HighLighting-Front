213c62d3fbdf1fbd572824f5f766eb53d0e7ca35
Lenol <ek3434@naver.com>
2022-03-05 02:16:42 +0900

    홈페이지 개선 - 안내문구, 가이드라인 포맷

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -71,19 +71,101 @@ export default function Home() {
   return (
     <div>
       <Header logged={logged} onLogout={onLogout} />
-      <div className="App-body">
-        <h1>HIGHLIGHTING</h1>
-        <input
-          className="InputBar"
-          ref={urlInput}
-          placeholder="다시보기 영상 URL을 입력해주세요"
-          onChange={onChangeUrl}
-          id="link"
-        />
-        <h3>URL : {url}</h3>
-        <button className="resultButton" onClick={linkCheck}>
-          <span>결과 보기</span>
-        </button>
+      <div className="HomeContainer">
+        <div className="Home_main">
+          <div className="Home_Column1">
+            <div>
+              <h1 className="HC1_heading">
+                <img className="HC1-logo" src={require("./image/icon.png")} />
+                HIGHLIGHTING
+              </h1>
+              <p className="HC1-p1">
+                저희 웹 서비스{" "}
+                <span className="point1">하이라이팅(HIGHLIGHTING)</span>은 영상
+                url만을 통해 해당 영상을 분석하여 편집점을 찾는데 도움이 되는
+                데이터와 서비스들을 제공합니다.
+              </p>
+              <p className="HC1-p1">
+                긴 시간이 걸리는 영상 원본의 편집접 찾기를 저희 웹서비스{" "}
+                <span className="point1">하이라이팅</span>이 제공하는 데이터와
+                서비스를 통해 빠른시간에 처리해보세요!{" "}
+              </p>
+              <p className="HC1-p2">
+                {" "}
+                홈페이지 하단부분에
+                <span className="point1"> 가이드라인</span>에 단계별 사용법을
+                기재해 놨습니다. 사용방법으로 빠르게 이동하길 원하신다면{" "}
+                <span className="point2">클릭</span>
+                해주세요!
+              </p>
+              <p className="HC1-p3">
+                Creating and Providing Services :{" "}
+                <span className="point1">Team HIGHLIGHTING</span>
+              </p>
+            </div>
+            <div className="Home_introduction_heading_wrap">
+              <h2> 가이드라인 </h2>
+            </div>
+            <ul class="Home_list">
+              <li class="Home_list-item">
+                <div>
+                  <p class="Home_list-content">0단계</p>
+                </div>
+              </li>
+
+              <li class="Home_list-item">
+                <div class="Home_list-content-wrap">
+                  <p class="Home_list-content">1단계</p>
+                  <p class="Home_list-sub">1단계 내용</p>
+                </div>
+              </li>
+
+              <li class="Home_list-item">
+                <div class="Home_list-item-image"></div>
+                <div class="Home_list-content-wrap">
+                  <p class="Home_list-content">2단계</p>
+                  <p class="Home_list-sub">2단계 내용</p>
+                </div>
+              </li>
+
+              <li class="Home_list-item">
+                <div class="Home_list-content-wrap">
+                  <p class="Home_list-content">3단계</p>
+                  <p class="Home_list-sub">3단계 내용</p>
+                </div>
+              </li>
+
+              <li class="Home_list-item">
+                <div class="Home_list-content-wrap">
+                  <p class="Home_list-content">4단계</p>
+                  <p class="Home_list-sub">4단계 내용</p>
+                </div>
+              </li>
+
+              <li class="Home_list-item">
+                <div class="Home_list-item-image"></div>
+                <div class="Home_list-content-wrap">
+                  <p class="Home_list-content">5단계</p>
+                  <p class="Home_list-sub">5단계 내용</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="Home_Column2">
+            <h1>URL 입력창</h1>
+            <input
+              className="InputBar"
+              ref={urlInput}
+              placeholder="다시보기 영상 URL을 입력해주세요"
+              onChange={onChangeUrl}
+              id="link"
+            />
+            <h1>URL : {url}</h1>
+            <button className="resultButton" onClick={linkCheck}>
+              <span>결과 보기</span>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
