2ba5be2905e2d203f37b62e5cad17c113a0d6bb2
Lenol <ek3434@naver.com>
2022-03-10 15:42:18 +0900

    1안

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -122,32 +122,23 @@ const Home = () => {
                 HIGHLIGHTING
               </h1>
               <p className="HC1-p1">
-                장면 <span className="point3">하나하나 넘기며 확인</span>하고,{" "}
-                <br />
-                시청자 반응은 어땠는지{" "}
-                <span className="point3">채팅도 다시 체크</span>하고, <br />
-                <span className="point2"> 불편하지 않았나요?</span>
+                장면 하나하나 넘기며 확인하고, 시청자 반응은 어땠는지 채팅도
+                다시 체크 하고 불편하지 않았나요?
               </p>
               <p className="HC1-p1">
-                <span className="point1">하이라이팅(HIGHLIGHTING)</span>을
-                써보세요! <br />
-                방송의 <span className="point3">다시보기 링크</span>만 입력하면
-                이용할 수 있습니다
+                저희 웹서비스{" "}
+                <span className="point2">하이라이팅(HIGHLIGHTING)</span>을
+                써보세요! 방송의 다시보기 링크만 입력하면 이용할 수 있습니다
               </p>
               <p className="HC1-p2">
-                <span className="point2">
-                  화면과 볼륨의 변화, 채팅 빈도, 키워드 출현, 후원 통계
-                </span>{" "}
-                등<br />
-                길고 긴 방송 속에서 특별한 부분들을 찾는 기능들을 제공하고
-                <br />
-                <span className="point2">필요한 장면만 골라</span> 가져갈 수
-                있어요
+                화면의 변화, 채팅 빈도, 키워드 출현 빈도, 후원 통계 등 길고 긴
+                방송 속에서 특별한 부분들을 찾는 기능들을 제공하고 필요한 장면만
+                골라 가져갈 수 있어요
               </p>
               <p className="HC1-p3">
                 {" "}
                 하단 <span className="point1">가이드라인</span> 에서 사용법을
-                알아보세요 :
+                알아보세요 = &gt;
                 <span className="point2" onClick={viewChange}>
                   클릭!
                 </span>
